f4bf742a631a6aa14935053f228d84d5d6b66490
Egor Kostiuk <kostukegor729@gmail.com>
2024-10-26 17:59:09 +0300

    feat: add product service

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -1,6 +1,16 @@
+import axios from 'axios'
+import { IProductsResponse } from '../types/product.interface'
+
+axios.defaults.baseURL = 'https://dummyjson.com'
+
 export const ProductService = {
-  getProducts: async() => {
-    const response = await fetch('https://dummyjson.com/products')
-    return response.json()
-  }
+	getProducts: async () => {
+		const response = await axios.get<IProductsResponse>('/products', {
+      params: {
+        limit: 5
+      }
+    })    
+    
+		return response.data
+	},
 }
