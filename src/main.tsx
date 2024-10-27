90324dc0bd0b5e199b17ad72aa41c4042135f357
Egor Kostiuk <kostukegor729@gmail.com>
2024-10-27 21:43:00 +0200

    feat: add routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,33 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
 import { Home } from './components/pages/home/Home.tsx'
+
 import './index.css'
 
 const queryClient = new QueryClient()
 
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Home></Home>,
+	},
+	{
+		path: '/cart',
+		// element: <Cart />,
+	},
+	{
+		path: '/product',
+		// element: <Product />
+	},
+])
+
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
 		<QueryClientProvider client={queryClient}>
-			<Home />
+			<RouterProvider router={router} />
 		</QueryClientProvider>
 	</StrictMode>,
 )
