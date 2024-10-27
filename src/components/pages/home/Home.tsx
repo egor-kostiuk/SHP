a954aa1fc59e2a36392c500a27a9d5ccaa57271a
Egor Kostiuk <kostukegor729@gmail.com>
2024-10-27 20:01:50 +0200

    update: Home page

--- src/components/pages/home/Home.tsx
+++ src/components/pages/home/Home.tsx
@@ -1,53 +1,36 @@
-import { FC, useEffect, useState } from 'react';
-import { ProductService } from '../../../services/product.service';
+import { FC } from 'react'
+import { useQuery } from '@tanstack/react-query'
+import { Layout } from '../../ui/layout/Layout'
+import { ProductService } from '../../../services/product.service'
+import { ProductItem } from '../../ui/product-item/ProductItem'
 
-import styles from './Home.module.scss';
+import styles from './Home.module.scss'
 
 export const Home: FC = () => {
-	const [products, setProducts] = useState([]);
-	const [error, setError] = useState('');
-	const [isLoading, setLoading] = useState(true);
-
-	// Promise
-	/*
-  useEffect(() => {
-    ProductService.getProducts()
-    .then(data => setProducts(data.products))
-    .catch(error => setError(error))
-    .finally(() => setLoading(false));
-  }, []);
-  */
-
-	useEffect(() => {
-		const fetch = async () => {
-			try {
-				const { products } = await ProductService.getProducts();
-				setProducts(products);
-			} catch (error: any) {
-				setError(error);
-			} finally {
-				setLoading(false);
-			}
-		};
-
-		fetch();
-	}, []);
+	const {
+		data: products,
+		error,
+		isLoading,
+	} = useQuery({
+		queryKey: ['products'],
+		queryFn: () => ProductService.getProducts(),
+		select: ({ products }) => products,
+	})
 
 	return (
-		<div className={styles.bg}>
+		<Layout title={'Shope the collection'}>
 			{error && <div className="text-red-500 bg-black">Error</div>}
 			{isLoading ? (
 				<div className="text-green-400 bg-black">Loading...</div>
-			) : products.length ? (
-				products.map(product => (
-					<div>
-						{/* @ts-ignore */}
-						{product.title}
-					</div>
-				))
+			) : products?.length ? (
+				<div className={styles.wrapper}>
+					{products.map(product => (
+						<ProductItem key={product.id} product={product} />
+					))}
+				</div>
 			) : (
 				<div>Products not Found!</div>
 			)}
-		</div>
-	);
-};
+		</Layout>
+	)
+}
