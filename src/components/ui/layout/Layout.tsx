0d9f1e47f5fe11610e09280e80db69897d05155f
Egor Kostiuk <kostukegor729@gmail.com>
2024-10-27 20:01:15 +0200

    feat: add layout

---
+++ src/components/ui/layout/Layout.tsx
@@ -0,0 +1,15 @@
+import { FC, PropsWithChildren } from 'react'
+
+import styles from './Layout.module.scss'
+
+export const Layout: FC<PropsWithChildren<{ title?: string }>> = ({
+	children,
+	title,
+}) => {
+	return (
+		<div className={styles.layout}>
+			{title && <h1 className={styles.heading}>{title}</h1>}
+			{children}
+		</div>
+	)
+}
