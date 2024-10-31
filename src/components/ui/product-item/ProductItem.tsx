032be8e9a41e0ff5bca77c3efab602711ddd334d
Egor Kostiuk <kostukegor729@gmail.com>
2024-10-31 21:27:12 +0200

    update: product item

--- src/components/ui/product-item/ProductItem.tsx
+++ src/components/ui/product-item/ProductItem.tsx
@@ -2,11 +2,14 @@ import { FC } from 'react'
 import { IProduct } from '../../../types/product.interface'
 
 import styles from './ProductItem.module.scss'
+import { Link } from 'react-router-dom'
 
 export const ProductItem: FC<{ product: IProduct }> = ({ product }) => {
 	return (
 		<div className={styles.item}>
-			<img src={product.thumbnail} alt={product.title} />
+			<Link to={`/product/${product.id}`}>
+				<img src={product.thumbnail} alt={product.title} />
+			</Link>
 			<div className={styles.heading}>{product.title}</div>
 			<div className={styles.price}>
 				{new Intl.NumberFormat('en-Us', {
