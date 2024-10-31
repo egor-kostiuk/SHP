0cb9ebc291fd85578ac15362411e3c893fc9e07d
Egor Kostiuk <kostukegor729@gmail.com>
2024-10-31 21:28:01 +0200

    update: product service

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -1,16 +1,21 @@
 import axios from 'axios'
-import { IProductsResponse } from '../types/product.interface'
+import { IProduct, IProductsResponse } from '../types/product.interface'
 
-axios.defaults.baseURL = 'https://dummyjson.com'
+axios.defaults.baseURL = 'https://dummyjson.com/products'
 
 export const ProductService = {
 	getProducts: async () => {
-		const response = await axios.get<IProductsResponse>('/products', {
-      params: {
-        limit: 5
-      }
-    })    
-    
+		const response = await axios.get<IProductsResponse>('/', {
+			params: {
+				limit: 5,
+			},
+		})
+
+		return response.data
+	},
+
+	getProductById: async (id: string) => {
+		const response = await axios.get<IProduct>(`/${id}`)
 		return response.data
 	},
 }
