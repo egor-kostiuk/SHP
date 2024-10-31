40ef4f20cf81007f3d9af3648042d22ab578d01d
Egor Kostiuk <kostukegor729@gmail.com>
2024-10-31 20:19:11 +0200

    feat: add Cart & Product pages

---
+++ src/components/pages/cart/Cart.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+export const Cart: FC = () => {
+  return (
+    <div></div>
+  )
+}
---
+++ src/components/pages/product/Product.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react'
+
+export const Product: FC = () => {
+  return (
+    <div></div>
+  )
+}
