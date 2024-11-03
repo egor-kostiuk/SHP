a08589f3876b767c92ced099d5c96c798c2614aa
Egor Kostiuk <kostukegor729@gmail.com>
2024-11-03 22:40:06 +0200

    feat: add Product page

--- src/components/pages/product/Product.tsx
+++ src/components/pages/product/Product.tsx
@@ -1,7 +1,26 @@
 import { FC } from 'react'
+import { Layout } from '../../ui/layout/Layout'
+import { useQuery } from '@tanstack/react-query'
+import { ProductService } from '../../../services/product.service'
+import { useParams } from 'react-router-dom'
 
 export const Product: FC = () => {
-  return (
-    <div></div>
-  )
+	const params = useParams()
+
+	const {
+		data: product,
+		error,
+		isLoading,
+	} = useQuery({
+		queryKey: ['product', params.id],
+		queryFn: () => ProductService.getProductById(params.id || ''),
+	})
+
+	return (
+		<Layout>
+			{isLoading && <div>Loading...</div>}
+
+			{<div>{product?.title}</div>}
+		</Layout>
+	)
 }
